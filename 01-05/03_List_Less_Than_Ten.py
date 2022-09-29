36fef02596135ff642cba5eb6adb5f6ee4e34104
Blandes22 <96037855+Blandes22@users.noreply.github.com>
2022-09-29 15:22:14 -0500

    Delete 01-05 directory

--- 01-05/03_List_Less_Than_Ten.py
+++
@@ -1,6 +0,0 @@
-#Asks user for a number then prints the list with all numbers lower than their number
-
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-n = int(input("Please enter a number: "))
-b= [i for i in a if i < n]
-print(b)
