cec8edcf4ff267ebecd486b3fbadb9a802b3de46
Blandes22 <96037855+Blandes22@users.noreply.github.com>
2022-09-29 15:22:24 -0500

    Delete 06-10 directory

--- 06-10/06_String_Lists.py
+++
@@ -1,9 +0,0 @@
-#checks if any given string is a palindrome
-
-str = input("Please enter a string: ")
-
-#str[::-1] returns the string in reverse
-if str == str[::-1]:
-    print("Your string is a palindrome.")
-else:
-    print("Your string is not a palindrome.")
--- 06-10/07_List_Comprehensions.py
+++
@@ -1,5 +0,0 @@
-#Takes a list and returns only the even values from it
-
-a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
-b = [x for x in a if x % 2 == 0]
-print(b)
